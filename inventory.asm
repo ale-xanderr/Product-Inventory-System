362149c921acc41d59245338581d25771525e889
ale-xanderr <seaquino@my.cspc.edu.ph>
2025-10-29 17:11:21 +0800

    Update inventory.asm

---
+++ inventory.asm
@@ -0,0 +1,18 @@
+section .data
+    header db "================================", 10, \
+            "   PRODUCT INVENTORY SYSTEM", 10, \
+            "================================", 10, 0
+
+    menu db "[1] Add Product", 10, \
+        "[2] Delete Product by Name", 10, \
+        "[3] Delete All Products with Zero Stock", 10, \
+        "[4] Search Product by Name", 10, \
+        "[5] Search Low-Stock Product", 10, \
+        "[6] Display All Products", 10, \
+        "[7] Display Products Sorted by Quantity", 10, \
+        "[8] Exit", 10, \
+        "================================",10, 0
+        
+    choice db 
+section .bss
+section .text
